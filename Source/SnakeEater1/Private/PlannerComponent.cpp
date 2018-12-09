0ed1de5babdf3eb9bb7da35f340fdacf46d435b4
MitchellKaysen <kaysenmitchell@gmail.com>
2018-12-09 16:05:31 -0600

    Removing redundancies
    Scrapped irrelevant files that I accidentally made and moved all of the functionality of UPlanner into UPlannerComponent

--- Source/SnakeEater1/Private/PlannerComponent.cpp
+++ Source/SnakeEater1/Private/PlannerComponent.cpp
@@ -9,11 +9,10 @@ UPlannerComponent::UPlannerComponent(const FObjectInitializer& ObjectInitializer
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	Planner = NewObject<UPlanner>();
 	UTestActionCover* testAction = NewObject<UTestActionCover>();
 	UTestActionChain* testAction2 = NewObject<UTestActionChain>();
-	Planner->AddAction(testAction);
-	Planner->AddAction(testAction2);
+	AddAction(testAction);
+	AddAction(testAction2);
 	needsPlan = true;
 	/*
 	TSharedPtr<UTestActionCover> testAction = TSharedPtr<UTestActionCover>(NewObject<UTestActionCover>());
@@ -42,16 +41,112 @@ void UPlannerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 		UE_LOG(LogTemp, Warning, TEXT("NO ACTION SET"));
 		FPlannerWorldState test_GoalState;
 		test_GoalState.Properties.Add(FWorldProperty(EPlannerSymbol::k_TESTSYM4, true));
-		if (Planner != nullptr)
+		
+		bool success = SearchForGoal(test_GoalState);
+		if (success)
 		{
-			bool success = Planner->SearchForGoal(test_GoalState);
-			if (success)
-			{
-				Plan = Planner->SearchResultOnSuccess;
-			}
+			Plan = SearchResultOnSuccess;
 		}
 	}
 
 	// ...
 }
 
+void UPlannerComponent::AddAction(UGOAPAction *action)
+{
+	ActionTable.Add(action);
+	for (auto& e : action->Effects)
+	{
+		EffectActionMap.Add(e.Key, numActions);
+	}
+	numActions++;
+}
+
+bool UPlannerComponent::SearchForGoal(FPlannerWorldState GoalConditions)
+{
+	UE_LOG(LogTemp, Warning, TEXT("NumActions:%d"), ActionTable.Num())
+		for (auto& p : GoalConditions.Properties)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("%d | %d"), static_cast<uint32>(p.key), p.value)
+		}
+
+	//goal state and targetstate are expressed in terms of the same properties...
+	FPlannerNode *CurrentNode = new FPlannerNode();
+	CurrentNode->Unsatisfied = GoalConditions;
+	CurrentNode->cost = 0;
+	CurrentNode->action = numActions;
+	CurrentNode->isTerminalNode = true;
+	TArray<FPlannerNode> Fringe;
+	TSet<uint32> ClosedSet;
+	Fringe.Heapify();
+	bool isFringeEmpty = false;
+
+	//A* search from goal state
+	//closed set (action)
+	//fringe (Planner Nodes)
+	/*while (current is not target and fringe is not empty){
+	current = fringe pop
+	fringe push (N, cost(N)) for Node N in succ(current)
+
+	successor(parent) : Node n foreach n not in Closed iff exists edge(parent, n)
+	exists edge(n0, n1) iff exists (k->v) in n1.effects s.t (e == pre), (k->e) in n1, (k->pre) in n0
+	}
+	return current
+	*/
+	while (!isFringeEmpty)
+	{
+		if (CurrentNode->HasReachedTarget()) {
+			//TODO: cache result
+			SearchResultOnSuccess = CurrentNode;
+			return true;
+		}
+		UE_LOG(LogTemp, Warning, TEXT("Current Num: %d"), Fringe.Num())
+			ClosedSet.Add(CurrentNode->action);
+
+
+		TSet< FPlannerNode, NodeKeyFuncs > children;
+		for (auto& Precond : CurrentNode->Unsatisfied.Properties)
+		{
+
+			//find actions that MIGHT satisfy effects (will change to TMultiMap so multiple actions can have same effect)
+			ActionID* PotentialAction = EffectActionMap.Find(Precond.key);
+			if (PotentialAction != nullptr)
+			{
+				UE_LOG(LogTemp, Error, TEXT("Looking at action %d"), *PotentialAction)
+					UGOAPAction* action = ActionTable[*PotentialAction];
+				FWorldProperty* p = action->Effects.Find(Precond.key);
+				if (p) {
+					UE_LOG(LogTemp, Error, TEXT("Action prop: %d | %d"), static_cast<uint32>(p->key), p->value)
+						UE_LOG(LogTemp, Error, TEXT("Precondition: %d | %d"), static_cast<uint32>(Precond.key), Precond.value)
+
+						if (Precond == *p && action->IsContextSatisfied())
+						{
+							//check to see if we already a made a child node for this action
+							FPlannerNode* newChild = new FPlannerNode();
+							newChild->action = *PotentialAction;
+
+							if (!ClosedSet.Contains(*PotentialAction))
+							{
+								newChild->ParentNode = CurrentNode;
+								newChild->cost = action->cost + CurrentNode->cost;
+								for (auto & pre : action->Preconditions)
+								{
+									newChild->Unsatisfied.Properties.Add(pre.Value);
+									newChild->cost++;
+								}
+								Fringe.HeapPush(*newChild);
+							}
+						}
+				}
+			}
+		}
+		isFringeEmpty = (Fringe.Num() == 0);
+		if (!isFringeEmpty)
+		{
+			Fringe.HeapPop(*CurrentNode);
+		}
+	}
+
+	SearchResultOnSuccess = nullptr;
+	return false;
+}
--- Source/SnakeEater1/Public/PlannerComponent.h
+++ Source/SnakeEater1/Public/PlannerComponent.h
@@ -3,11 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Planner.h"
 #include "Components/ActorComponent.h"
+
+#include "GOAPAction.h"
+#include "PlannerNode.h"
 #include "TestActionCover.h"
+
 #include "PlannerComponent.generated.h"
 
+
+typedef uint32 ActionID;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SNAKEEATER1_API UPlannerComponent : public UActorComponent
@@ -15,9 +20,9 @@ class SNAKEEATER1_API UPlannerComponent : public UActorComponent
 	GENERATED_UCLASS_BODY()
 
 private:
-	UPROPERTY()
-	UPlanner* Planner;
+
 	bool needsPlan;
+
 	UPROPERTY()
 	UGOAPAction* currentAction;
 
@@ -32,5 +37,18 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	uint32 numActions;
+
+	FPlannerNode * SearchResultOnSuccess;
+
+	UPROPERTY()
+		TArray<UGOAPAction*> ActionTable;
+
+	UPROPERTY()
+		TMap<EPlannerSymbol, uint32> EffectActionMap; //Assume one action per effect now but will need to expand this
+
+	void AddAction(UGOAPAction *action);
+	bool SearchForGoal(FPlannerWorldState GoalConditions);
 	
+
 };
